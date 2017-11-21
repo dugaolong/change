23cd51802a5e5f68c05637ab235d0c8dff0ed617
dugaolong <1872387570@qq.com>
2017-11-21 18:56:47 +0800

    添加信息

--- app/src/main/java/Test.java
+++ app/src/main/java/Test.java
@@ -8,12 +8,13 @@ import java.util.Random;
 public class Test {
 
     public static String aaa;
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
 //        aaa = "111";
 //        System.out.println("aaa:"+aaa);
 //        aaa = "222";
 //        System.out.println("aaa:"+aaa);
-        for (int i = 0; i<100;i++){
+        for (int i = 0; i < 100; i++) {
 //            System.out.println("add(\""+randomIMEI()+"\");");
 //           createRandomCharData(16);
 //            createRandomScreen();
@@ -30,10 +31,10 @@ public class Test {
         try {
             Class clazz = Class.forName(pkgName);
             Method[] methods = clazz.getMethods();
-            int iii=0;
+            int iii = 0;
             for (Method method : methods) {
                 String methodName = method.getName();
-                System.out.println("add(\""+methodName+"\");");
+                System.out.println("add(\"" + methodName + "\");");
 //                Class<?>[] parameterTypes = method.getParameterTypes();
 //                for (Class<?> clas : parameterTypes) {
 //                    String parameterName = clas.getName();
@@ -42,13 +43,15 @@ public class Test {
 //                System.out.println("*****************************");
                 iii++;
             }
-            System.out.println("count:"+iii);
+            System.out.println("count:" + iii);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    /**随机生成imei****/
+    /**
+     * 随机生成imei
+     ****/
     public static String randomIMEI() {// calculator IMEI
         int r1 = 1000000 + new java.util.Random().nextInt(9000000);
         int r2 = 1000000 + new java.util.Random().nextInt(9000000);
@@ -79,21 +82,18 @@ public class Test {
     //0~9的ASCII为48~57
     //A~Z的ASCII为65~90
     //a~z的ASCII为97~122
-    public static void createRandomCharData(int length)
-    {
-        StringBuilder sb=new StringBuilder();
-        Random rand=new Random();//随机用以下三个随机生成器
-        Random randdata=new Random();
-        int data=0;
-        for(int i=0;i<length;i++)
-        {
+    public static void createRandomCharData(int length) {
+        StringBuilder sb = new StringBuilder();
+        Random rand = new Random();//随机用以下三个随机生成器
+        Random randdata = new Random();
+        int data = 0;
+        for (int i = 0; i < length; i++) {
 //            int index=rand.nextInt(3);
-            int index=rand.nextInt(2);
+            int index = rand.nextInt(2);
             //目的是随机选择生成数字，大小写字母
-            switch(index)
-            {
+            switch (index) {
                 case 0:
-                    data=randdata.nextInt(10);//仅仅会生成0~9
+                    data = randdata.nextInt(10);//仅仅会生成0~9
                     sb.append(data);
                     break;
 //                case 1:
@@ -101,59 +101,64 @@ public class Test {
 //                    sb.append((char)data);
 //                    break;
                 case 1:
-                    data=randdata.nextInt(26)+97;//保证只会产生97~122之间的整数
-                    sb.append((char)data);
+                    data = randdata.nextInt(26) + 97;//保证只会产生97~122之间的整数
+                    sb.append((char) data);
                     break;
             }
         }
-        String result=sb.toString();
-        System.out.println("add(\""+result+"\");");
+        String result = sb.toString();
+        System.out.println("add(\"" + result + "\");");
     }
-    public static void createRandomScreen()
-    {
-        Random rand=new Random();//随机用以下三个随机生成器
-        int index=rand.nextInt(2);
+
+    public static void createRandomScreen() {
+        Random rand = new Random();//随机用以下三个随机生成器
+        int index = rand.nextInt(2);
         //目的是随机选择生成数字，大小写字母
-        switch(index)
-        {
+        switch (index) {
             case 0:
-                System.out.println("add(new ScreenInfo(\"1080\",\"1920\",\"3.0\"));"); break;
+                System.out.println("add(new ScreenInfo(\"1080\",\"1920\",\"3.0\"));");
+                break;
             case 1:
-                System.out.println("add(new ScreenInfo(\"720\",\"1280\",\"2.0\"));"); break;
+                System.out.println("add(new ScreenInfo(\"720\",\"1280\",\"2.0\"));");
+                break;
         }
     }
 
-    public static void createRandomVersion()
-    {
-        Random rand=new Random();//随机用以下三个随机生成器
-        int indexVersion=rand.nextInt(7);
+    public static void createRandomVersion() {
+        Random rand = new Random();//随机用以下三个随机生成器
+        int indexVersion = rand.nextInt(7);
         //目的是随机选择生成数字，大小写字母
-        switch(indexVersion)
-        {
+        switch (indexVersion) {
             case 0://
-                System.out.println("add(\"4.4.4\");"); break;
+                System.out.println("add(\"4.4.4\");");
+                break;
             case 1://
-                System.out.println("add(\"5.0.1\");"); break;
+                System.out.println("add(\"5.0.1\");");
+                break;
             case 2://
-                System.out.println("add(\"5.0.2\");"); break;
+                System.out.println("add(\"5.0.2\");");
+                break;
             case 3://
-                System.out.println("add(\"5.1.1\");"); break;
+                System.out.println("add(\"5.1.1\");");
+                break;
             case 4://
-                System.out.println("add(\"6.0\");"); break;
+                System.out.println("add(\"6.0\");");
+                break;
             case 5://
-                System.out.println("add(\"6.0.1\");"); break;
+                System.out.println("add(\"6.0.1\");");
+                break;
             case 6://
-                System.out.println("add(\"7.0\");"); break;
+                System.out.println("add(\"7.0\");");
+                break;
         }
 
     }
-    public static void createRandomPhone()
-    {
-        Random rand=new Random();//随机用以下三个随机生成器
-        int indexMake=rand.nextInt(2);
+
+    public static void createRandomPhone() {
+        Random rand = new Random();//随机用以下三个随机生成器
+        int indexMake = rand.nextInt(2);
         //目的是随机选择生成数字，大小写字母
-        switch(indexMake)
-        {
+        switch (indexMake) {
             case 0://xiaomi
                 System.out.println("add(new PhoneInfo(\"Redmi Note 3\",\"kenzo\",\"6.0.1\",\"V8.5.5.0.MHOCNED\",\"xiaomi\"));");
                 break;
